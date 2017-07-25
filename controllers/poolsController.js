8ca016c59947d9a76328daa780c83e2cf3cee2cf
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-25 15:10:38 -0700

    completed setting up poolsController index route to retrieve all pools from database. Confirmed working.

--- controllers/poolsController.js
+++ controllers/poolsController.js
@@ -6,7 +6,16 @@ let db = require('../models');
 function index (req, res) {
   // this will return all POOLS
   console.log('This route shows all Pools.');
-  res.json();
+
+  // Find all objects in the database that are in the Pool Schema.
+  db.Pool.find({}, function (err, pool) {
+
+      if (err) {
+        console.log("Error", err);
+      }
+      res.json(pool);
+  });
+
 }
 
 function show (req, res) {
