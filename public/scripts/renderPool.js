e01b3da5a9b553e445fc012794c5fc6806d4e6e4
sonoilconte <shammersinger@yahoo.com>
2017-07-28 08:02:21 -0700

    cleanup of bootstrap layout

--- public/scripts/renderPool.js
+++ public/scripts/renderPool.js
@@ -5,25 +5,21 @@ function renderPool(pool){
 
   let poolHTML = `
           <!--Beginning of Pool Data-->
-          <div class="pool row" data-pool-id="${pool._id}">
-
-            <!--Pool Info Column Begin-->
-            <div class="col-sm-6 col-md-4">
-
-              <div class="pool-header clearfix">
-              <button class="pool-delete-btn">Delete Pool</button>
-              <h2>${pool.name}</h2>
-              </div>
-
-              <section class="contact">
-                <p>${pool.address}</p>
-                <p>${pool.phoneNumber}</p>
-                <p>${pool.contactEmail}</p>
-              </section>
-
-              <section class="op-hours">
+          <div class="pool col-xs-12" data-pool-id="${pool._id}">
+            <div class="row">
+              <!--Pool Info Column Begin-->
+              <div class="col-sm-6 col-md-4">
+                <div class="pool-header clearfix">
+                  <button class="pool-delete-btn">Delete Pool</button>
+                  <h2>${pool.name}</h2>
+                </div>
+                <section class="contact">
+                  <p>${pool.address}</p>
+                  <p>${pool.phoneNumber}</p>
+                  <p>${pool.contactEmail}</p>
+                </section>
+                <section class="op-hours">
                   <h3>Hours of Operation</h3>
-
                   <div class="row">
                     <p class="col-sm-6">Monday</p>
                     <p class="col-sm-6">${pool.monday}</p>
@@ -52,57 +48,45 @@ function renderPool(pool){
                     <p class="col-sm-6">Sunday</p>
                     <p class="col-sm-6">${pool.sunday}</p>
                   </div>
-              </section>
-            </div>
-            <!--Pool Info Column End-->
-
-            <!-- Pool Info Column Begin-->
-            <div class="col-sm-6 col-md-4">
-
-              <section class="events">
-                <h3>Events and Schedule Alerts</h3>
-                <div data-pool-events-id='${pool._id}'>
-                </div>
-
-                <!-- ADD EVENT FORM-->
-                <div class="add-event">
-                  <h4>Add an Event</h4>
-
-                  <form action="index.html" method="post">
-
-                    <div class="row">
-                      <div class="col-sm-6"><input type="text" name="dayOfWeek" value="" placeholder="Day of Week"></div>
-                      <div class="col-sm-6"><input type="text" name="title"  value="" placeholder="Title"></div>
-                    </div>
-
-                    <div class="row">
-                      <div class="col-sm-6"><input type="text" name="startTime" value="" placeholder="Start Time"></div>
-                      <div class="col-sm-6"><input type="text" name="endTime" value="" placeholder="End Time"></div>
-                    </div>
-
-                    <div class="row">
-                      <div class="col-sm-6"><input type="text" name="description" value="" placeholder="Description"></div>
-                      <div class="col-sm-6"><input type="submit" name="submit" value="Submit"></div>
-                    </div>
-                  </form>
-
-                </div>
-
-              </section>
-            </div>
-          <!-- Pool Info Column End -->
+                </section>
+              </div> <!--Pool Info Column End-->
+              <div class="col-sm-6 col-md-4"> <!-- Pool Info Column Begin-->
+                <section class="events">
+                  <h3>Events and Schedule Alerts</h3>
+                  <div data-pool-events-id='${pool._id}'>
+                  </div>
+                  <!-- ADD EVENT FORM-->
+                  <div class="add-event">
+                    <h4>Add an Event</h4>
+                    <form action="index.html" method="post">
+                      <div class="row">
+                        <div class="col-sm-6"><input type="text" name="dayOfWeek" value="" placeholder="Day of Week"></div>
+                        <div class="col-sm-6"><input type="text" name="title"  value="" placeholder="Title"></div>
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-6"><input type="text" name="startTime" value="" placeholder="Start Time"></div>
+                        <div class="col-sm-6"><input type="text" name="endTime" value="" placeholder="End Time"></div>
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-6"><input type="text" name="description" value="" placeholder="Description"></div>
+                        <div class="col-sm-6"><input type="submit" name="submit" value="Submit"></div>
+                      </div>
+                    </form>
+                  </div>
+                </section>
+              </div>
+              <!-- Pool Info Column End -->
 
 
-          <!--Pool Info Map Section Begin-->
-            <div class="col-sm-6 col-md-4">
-              <section class="map">
+              <!--Pool Info Map Section Begin-->
+              <div class="col-sm-6 col-md-4">
+                <section class="map">
                 <div id="dummy-map">
-
-                </div>
-              </section>
+              </div>
+            </section>
             </div>
-          <!-- Pool Info Map Section End-->
-
+            <!-- Pool Info Map Section End-->
+          </div>
         </div>
         <!--Pool Info End -->`;
 
